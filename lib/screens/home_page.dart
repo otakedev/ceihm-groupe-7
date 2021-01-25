e572d6c6e77a7afbc297d2629f3e04ba29c718b0
YounesAbdennadher <younes.abdennadher@etu.univ-cotedazur.fr>
2021-01-25 11:16:18 +0100

    Added a keyboard visibility component. Made the scan button disappear when the user types something in the search bar.

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+import 'package:alergo/components/keyboard_visibility_builder.dart';
 import 'package:alergo/core/router.dart';
 import 'package:alergo/models/product_model.dart';
 import 'package:alergo/screens/product_page/product_page.dart';
@@ -11,12 +12,21 @@ import 'package:flappy_search_bar/search_bar_style.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   static const routeName = '/home';
 
   const HomePage({Key key}) : super(key: key);
 
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   Future<List<ProductModel>> search(String search) async {
+    if (search == "empty") {
+      return [];
+    }
+
     return ProductModel.fromMock()
         .where((product) =>
             product.name.toUpperCase().contains(search.toUpperCase()))
@@ -29,33 +39,10 @@ class HomePage extends StatelessWidget {
       resizeToAvoidBottomPadding: false,
       body: Center(
         child: Stack(children: [
-          Container(
-            decoration: dropShadow(
-              color: colorSecondary,
-              shape: BoxShape.circle,
-            ),
-            alignment: Alignment.center,
-            child: ClipOval(
-              child: Material(
-                color: colorSecondary,
-                child: InkWell(
-                  splashColor: colorPrimary,
-                  onTap: () => navigateToPage(context, ScannerPage()),
-                  child: AspectRatio(
-                    aspectRatio: 1 / 1,
-                    child: Padding(
-                      padding: const EdgeInsets.all(42.0),
-                      child: SvgPicture.asset(
-                        'assets/qrcodescan.svg',
-                        fit: BoxFit.contain,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
           SearchBar<ProductModel>(
+            emptyWidget: Center(
+              child: Text("Aucun résultat"),
+            ),
             hintText: "Rechercher",
             searchBarPadding: EdgeInsets.all(50).copyWith(top: 100),
             minimumChars: 1,
@@ -74,6 +61,42 @@ class HomePage extends StatelessWidget {
                 },
               );
             },
+          ),
+          // ),
+          KeyboardVisibilityBuilder(
+            builder: (context, child, isKeyboardVisible) {
+              if (isKeyboardVisible) {
+                return const SizedBox.shrink();
+              } else {
+                return child;
+              }
+            },
+            child: Container(
+              decoration: dropShadow(
+                color: colorSecondary,
+                shape: BoxShape.circle,
+              ),
+              alignment: Alignment.center,
+              child: ClipOval(
+                child: Material(
+                  color: colorSecondary,
+                  child: InkWell(
+                    splashColor: colorPrimary,
+                    onTap: () => navigateToPage(context, ScannerPage()),
+                    child: AspectRatio(
+                      aspectRatio: 1 / 1,
+                      child: Padding(
+                        padding: const EdgeInsets.all(42.0),
+                        child: SvgPicture.asset(
+                          'assets/qrcodescan.svg',
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ), // this widget goes to the builder's child property. Made for better performance.
           ),
         ]),
       ),
