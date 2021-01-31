cc8b9656da2a1485c64e19661895d8f403e964ea
Jason Haenlin <jason.haenlin@etu.univ-cotedazur.fr>
2021-01-31 13:39:02 +0100

    [#21] page to modifiy the profile of the user - move up the profile provider up in the tree to make it accessible by the profile creation page and the profile update page. - profile information are in memory for the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
+import 'package:alergo/providers/profile_selector_notifier.dart';
 import 'package:alergo/screens/home_page.dart';
 import 'package:alergo/screens/product_page/product_page.dart';
 import 'package:alergo/screens/profile_pages/profile_page.dart';
 import 'package:alergo/screens/search_page/search_page.dart';
+import 'package:alergo/screens/profile_pages/update_profile_page.dart';
 import 'package:alergo/screens/start_page/start_page.dart';
 import 'package:alergo/theme/style.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    ChangeNotifierProvider<ProfileSelectorNotifier>(
+      create: (_) => ProfileSelectorNotifier(),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -16,13 +24,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Alergo Demo',
       theme: appTheme(),
-      initialRoute: StartPage.routeName,
+      initialRoute: HomePage.routeName,
       routes: {
         HomePage.routeName: (context) => HomePage(),
         StartPage.routeName: (context) => StartPage(),
         ProfilePage.routeName: (context) => ProfilePage(),
         ProductPage.routeName: (context) => ProductPage(),
         SearchBarPage.routeName: (context) => SearchBarPage(),
+        UpdateProfilePage.routeName: (context) => UpdateProfilePage(),
       },
     );
   }
--- lib/screens/profile_pages/components/profile_tab_page.dart
+++ lib/screens/profile_pages/components/profile_tab_page.dart
@@ -1,41 +1,40 @@
 import 'package:alergo/models/profile_item_block_model.dart';
 import 'package:alergo/screens/profile_pages/components/profile_item_block.dart';
-import 'package:alergo/screens/profile_pages/profile_selector_notifier.dart';
+import 'package:alergo/providers/profile_selector_notifier.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class ProfileTabPage extends StatelessWidget {
   const ProfileTabPage({
     Key key,
-    this.label = "",
+    this.label,
     @required this.items,
     @required this.profileType,
-    @required this.pageController,
   })  : assert(profileType != null),
-        assert(pageController != null),
         super(key: key);
 
   final String label;
   final List<ProfileItemBlockModel> items;
   final ProfileType profileType;
-  final PageController pageController;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Padding(
-        padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.05)
-            .copyWith(bottom: 0),
+        padding: EdgeInsets.symmetric(
+          horizontal: MediaQuery.of(context).size.width * 0.05,
+        ),
         child: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 20),
-              child: Text(
-                label,
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
+            if (label != null)
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Text(
+                  label,
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
               ),
-            ),
             Flexible(
               child: GridView.builder(
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
@@ -53,7 +52,6 @@ class ProfileTabPage extends StatelessWidget {
                 },
               ),
             ),
-            SizedBox(height: 80)
           ],
         ),
       ),
