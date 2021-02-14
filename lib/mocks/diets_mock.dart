2ec9508d59ecea9ee1dea824dab20e2bb05dea82
Betsara-MARCELLIN <betsara.marcellin@etu.univ-cotedazur.fr>
2021-02-14 17:49:37 +0100

    #60 good diet name and illustrations & ingredients illustration

--- lib/mocks/diets_mock.dart
+++ lib/mocks/diets_mock.dart
@@ -4,67 +4,78 @@ const DIETS_MOCK = [
   {
     "id": 1,
     "name": "Végétarien",
-    "assetPath": "assets/diets/vegan.jpg",
+    "assetPath": "assets/diets/vegetarian.jpg",
     "forbiddenIngredients": [
       INGREDIENT_BEEF_MOCK,
       INGREDIENT_CHICKEN_MOCK,
     ],
     "compatibleIngredients": [
-      INGREDIENT_SALADE_MOCK,
-      INGREDIENT_TOFU_MOCK,
     ],
     "uncompatibleIngredients": [],
   },
-  {
+    {
     "id": 2,
-    "name": "Végétarien pro",
-    "assetPath": "assets/diets/vegan.jpg",
+    "name": "Pesco-végétarien",
+    "assetPath": "assets/diets/pesco-vegetarian.jpg",
     "forbiddenIngredients": [
-      INGREDIENT_BEEF_MOCK,
-      INGREDIENT_CHICKEN_MOCK,
     ],
     "compatibleIngredients": [
-      INGREDIENT_SALADE_MOCK,
-      INGREDIENT_TOFU_MOCK,
     ],
     "uncompatibleIngredients": [],
   },
-  {
+      {
     "id": 3,
-    "name": "Végétarien pro+",
+    "name": "Végan",
     "assetPath": "assets/diets/vegan.jpg",
     "forbiddenIngredients": [
       INGREDIENT_BEEF_MOCK,
       INGREDIENT_CHICKEN_MOCK,
+      INGREDIENT_CHICKEN_MOCK,
+      INGREDIENT_EGG_MOCK
     ],
     "compatibleIngredients": [
-      INGREDIENT_SALADE_MOCK,
-      INGREDIENT_TOFU_MOCK,
     ],
     "uncompatibleIngredients": [],
   },
   {
     "id": 4,
-    "name": "Végétarien pro++",
-    "assetPath": "assets/diets/vegan.jpg",
-    "forbiddenIngredients": [],
-    "compatibleIngredients": [],
+    "name": "Sans gluten",
+    "assetPath": "assets/diets/no-gluten.jpg",
+    "forbiddenIngredients": [
+    ],
+    "compatibleIngredients": [
+    ],
     "uncompatibleIngredients": [],
   },
   {
     "id": 5,
-    "name": "Végétarien max",
-    "assetPath": "assets/diets/vegan.jpg",
-    "forbiddenIngredients": [],
-    "compatibleIngredients": [],
+    "name": "Sans lactose",
+    "assetPath": "assets/diets/no-lactose.jpg",
+    "forbiddenIngredients": [
+      INGREDIENT_MILK_CHOCOLATE_MOCK
+    ],
+    "compatibleIngredients": [
+    ],
     "uncompatibleIngredients": [],
   },
   {
     "id": 6,
-    "name": "Végétarien max+",
-    "assetPath": "assets/diets/vegan.jpg",
-    "forbiddenIngredients": [],
-    "compatibleIngredients": [],
+    "name": "Cétogène",
+    "assetPath": "assets/diets/cetogen.jpg",
+    "forbiddenIngredients": [
+    ],
+    "compatibleIngredients": [
+    ],
+    "uncompatibleIngredients": [],
+  },
+  {
+    "id": 7,
+    "name": "Sans porc",
+    "assetPath": "assets/diets/no-pork.jpg",
+    "forbiddenIngredients": [
+    ],
+    "compatibleIngredients": [
+    ],
     "uncompatibleIngredients": [],
   },
 ];
